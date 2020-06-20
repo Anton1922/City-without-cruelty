231d99a7e2d057621e2075f4d379acb446a3b89c
Anton Fedorov <anton.pzkt@gmail.com>
2020-06-21 00:13:00 +0300

    deleted unnecessary code

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
   	dots: true,
   	dotsClass: 'cruelty-results__slider-dots',
   	centerMode: true,
-  	//centerPadding: '60px',
   	slidesToShow: 3,
   	slidesToScroll: 1,
   	responsive: [
@@ -30,37 +29,5 @@ $(document).ready(function(){
     }
   ]
   });
-
-  /*$('.cruelty-results__slider--socal-ad').slick({
-  	autoplay: true,
-  	autoplaySpeed: 2000,
-  	arrows: false,
-  	dots: true,
-  	dotsClass: 'cruelty-results__slider-dots',
-  	centerMode: true,
-  	//centerPadding: '60px',
-  	slidesToShow: 3,
-  	slidesToScroll: 1,
-  	responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 2
-      }
-    },
-    {
-      breakpoint: 500,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-  });*/
 });
 
